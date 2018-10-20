c29eb553172ee59e75d5b7f61074d8b756275165
aluno <aluno@LABMAC09.local>
2018-10-20 16:17:39 -0300

    started

---
+++ WebRequest/WebRequest/ListagemFotos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace WebRequest
+{
+    [DataContract]
+    public class ListagemFotos
+    {
+        [DataMember(Name = "photos")]
+        public List<string> Fotos;
+    }
+}
---
+++ WebRequest/WebRequest/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WebRequest
+{
+    public partial class MainPage : ContentPage
+    {
+
+        System.Net.WebRequest request;
+        public MainPage()
+        {
+            InitializeComponent();
+            status.Text = "carregando.";
+            Uri uri = new Uri("https://developer.xamarin.com/demo/stock.json");
+            request = System.Net.WebRequest.Create(uri);
+            request.BeginGetResponse(HandleAsyncCallback,null);
+        } 
+
+        void HandleAsyncCallback(IAsyncResult ar)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Stream stream = request.EndGetResponse(ar).GetResponseStream();
+                DataContractJsonSerializer serializer =
+                    new DataContractJsonSerializer(typeof(ListagemFotos));
+                ListagemFotos listagem = (ListagemFotos)serializer.ReadObject(stream);
+
+
+                listImagens.ItemsSource = listagem.Fotos;
+                status.IsVisible = false;
+
+            });
+        }
+
+    }
+}
